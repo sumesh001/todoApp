23794b329926a46f070fac9ede33132b7e54dfeb
sumesh0 <sumeshyogi07@gmail.com>
2024-05-14 20:07:55 +0530

    Initial commit

---
+++ ToDoApplication/src/main/java/com/demo/model/ToDo.java
@@ -0,0 +1,50 @@
+package com.demo.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String title;
+	
+	@DateTimeFormat(pattern="yyyy-mm-dd")
+	private  Date date;
+	private String status;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
